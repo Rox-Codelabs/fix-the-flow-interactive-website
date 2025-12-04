86dfd489f852d75f1b975ba347fa409f1131241d
Roxy <149881577+Rox-Codelabs@users.noreply.github.com>
2025-12-04 14:12:47 +0100

    #11 header animatie javascript en css

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
-// Hamburger menu open
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------Hamburger menu open
 // Stap 1
 let openNavButton = document.querySelector('.open-nav');
 // Stap 2
@@ -7,7 +8,7 @@ openNavButton.addEventListener('click', function(){
     document.querySelector('nav').classList.remove('hidden')
 });
 
-// Hamburger menu dicht
+// ---------------------------------Hamburger menu dicht
 // Stap 1
 let closeNavButton = document.querySelector('.close-nav');
 
@@ -16,3 +17,45 @@ closeNavButton.addEventListener('click', function(){
     //Stap 3
     document.querySelector('nav').classList.add('hidden')
 });
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------header logo weg na scrollen
+// Stap 1
+let headerLogo = document.querySelector('.civic-logo');
+let scrollArea = window;
+
+// Stap 2
+scrollArea.addEventListener('scroll', function(){
+    // Stap 3
+    headerLogo.classList.add('hidden');
+});
+
+
+// ---------------------------------header logo text tonen na scrollen
+//stap 1 
+let headerText = document.querySelector('.logo-text');
+
+//stap 2 
+scrollArea.addEventListener('scroll', function(){
+    //stap 3
+    headerText.classList.remove('hidden');
+});
+
+
+// ---------------------------------Als helemaal omhoog is gescrolled
+// Stap 1 hoeft niet meer
+// Stap 2
+window.addEventListener('scroll', function(){
+    if (window.scrollY === 0){
+        //Stap 3
+        headerLogo.classList.remove('hidden');
+        headerText.classList.add('hidden');
+    }
+});
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
+// --------------------------------
+
